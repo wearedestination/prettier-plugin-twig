d9474e7c521ed0f31c37e0a150a5712378f7e701
andyexeter <palmer.andy@gmail.com>
2026-03-06 11:36:30 +0000

    Attempting to fix build

--- build/shims.js
+++ build/shims.js
@@ -1,5 +1,7 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const grammarPath = path.join(__dirname, '../grammar');
 
